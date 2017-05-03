19aebbfaf020de23194713a141cf01baf2520abe
Anton Christensen <anton.christensen9700@gmail.com>
2017-05-03 21:35:47 +0200

    Fix path issue

--- scripts/lemonbar/lemonbar-start.sh
+++ scripts/lemonbar/lemonbar-start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-~/.files/lemonbar/lemon-conf.sh | lemonbar -g x32+0+0 -F "#f0f0f0" -B "#232c33" -f "Inconsolata:style=Regular:size=12" -f "FontAwesome:style=Regular:size=14" -d
+~/.files/scripts/lemonbar/lemon-conf.sh | lemonbar -g x32+0+0 -F "#f0f0f0" -B "#232c33" -f "Inconsolata:style=Regular:size=12" -f "FontAwesome:style=Regular:size=14" -d
 #~/.files/lemonbar/lemon-conf.sh | lemonbar -F "#f0f0f0" -B "#232c33" -g 1920x32+0+0 -f "Inconsolata:style=Regular:size=12" -f "FontAwesome:style=Regular:size=14"
 
